43a0f9efd52c35144a2582e2f8bd2a8a04ec35de
Daniel Figueroa <danfigueroa81@gmail.com>
2021-04-25 21:04:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import student_image from './resources/student_stock.jpeg';
+import professor_image from './resources/professor_stock.jpeg';
+import faculty_image from './resources/principal_stock.jpeg';
+import university_image from './resources/university_stock.jpeg';
+import students_interacting from './resources/students_interacting.jpeg'
+import laptop_image from './resources/laptop_stock.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <a class="navbar-brand" href="index.html">Rate My Scholar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="Register.html">Register</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.html">Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <main role="main">
+
+        <div class="container-fluid">
+          <div class="jumbotron jumbotron-fluid text-center" >
+            <h1 class="jumbotron-heading">Welcome to Rate My Scholar</h1>
+            <p class="text-muted">
+              With this complex and incredible site, you can peruse the quality of all your colleagues.
+              Not just your professor.
+              <br />
+              <h5>Begin your search by entering a school below.</h5>
+            </p>
+            <div class="input-group col-md-12">
+              <input class="form-control py-2" type="search" value="" id="example-search-input"></input>
+              <span class="input-group-append">
+                <button onclick="window.location.href='result.html';" class="btn btn-outline-secondary" type="button">
+                  <i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+          </div>
+          <div class="container marketing" style={{color: "white"}}>
+            <div class="row">
+              <div class="col-lg-4">
+                <img class="rounded-circle" src={student_image} alt="Generic placeholder image" width="150" height="150" />
+                <h2>Students</h2>
+                <p>This website will allow you to rate your fellow classmates or students of another school. This could be useful if you are deciding who is the best match for a group project.</p>
+                <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
+              </div>
+              <div class="col-lg-4">
+                <img class="rounded-circle" src={professor_image} alt="Generic placeholder image" width="150" height="150" />
+                <h2>Professors</h2>
+                <p>Peruse what colleagues and potentially other professors have to say on another professor to find the best mentors for your school career.</p>
+                <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
+              </div>
+              <div class="col-lg-4">
+                <img class="rounded-circle" src={faculty_image} alt="Generic placeholder image" width="150" height="160" />
+                <h2>Staff</h2>
+                <p>Check out the overall perception of the hard-working staff at your school or another who could help you in other matters.</p>
+                <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
+              </div>
+            </div>
+
+            <hr class="featurette-divider" />
+
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">Anyone from your school is likely already here. If not, you can change that.</h2>
+                <p class="lead">Rate My Scholar boasts a large database containing a myriad of information about most schools,
+                the students, professors and staff of that school. If for whatever reason a school, student, professor or staff member
+          is missing from the database, Rate My Scholar also gives you the oppurtunity to add a new entry to its massive database.</p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto"
+                  src={university_image}
+                  data-holder-rendered="true" />
+              </div>
+            </div>
+
+            <hr class="featurette-divider" />
+
+            <div class="row featurette">
+              <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading">Learn what your peers have to say on your fellow classmates' performance or perhaps that of a teacher's...</h2>
+                <p class="lead">Rate My Scholar is meant for the gauging and measuring of the quality of these individuals on a primarily professional level.
+                You will be able to read the reviews of whomever your search results in and this will be accompanied by their star rating and quality in their role.
+          This could be your greatest tool as you connect and journey through your university career.</p>
+              </div>
+              <div class="col-md-5 order-md-1">
+                <img class="featurette-image img-fluid mx-auto"
+                  src={students_interacting}
+                  data-holder-rendered="true"
+                />
+              </div>
+            </div>
+
+            <hr class="featurette-divider" />
+
+            <div class="row featurette">
+              <div class="col-md-7">
+                <h2 class="featurette-heading">No need to sign in to read and persue the reviews. Though you will need to in order to write one.</h2>
+                <p class="lead">Rate My Scholar does not demand that you create an account to use a majority of the services on this site. Though you will need to do so as reviews
+                require that they be made by verified accounts. If this if your first visit, you should press the "Register" link on the navigation bar. Otherwise, refer to the login page
+                if you already have an account.
+          </p>
+              </div>
+              <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" src={laptop_image} data-holder-rendered="true" />
+              </div>
+            </div>
+
+            <hr class="featurette-divider" />
+
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
